e0fb0213e93778803e129649d54027f9bc6399ec
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-05-15 18:34:37 -0500

    CORRECCION SPLASH
    Pantalla 2

--- app/src/main/java/com/daniela/ultimaentrega/MainActivity.java
+++ app/src/main/java/com/daniela/ultimaentrega/MainActivity.java
@@ -11,26 +11,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button entrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        entrar=findViewById(R.id.Ingresar);
-        entrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, Pantalla2.class);
-                startActivity(intent);
-            }
-        });
-
-
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent home = new Intent(MainActivity.this, MainActivity.class);
+                Intent home = new Intent(MainActivity.this, Pantalla2.class);
                 startActivity(home);
             }
         },4000);
