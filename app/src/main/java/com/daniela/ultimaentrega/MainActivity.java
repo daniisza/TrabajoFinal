42c7eb44f7aca41a7efa1f6b8b2e98787bf3679a
JesusUribe3 <chucho.jo33@gmail.com>
2023-05-15 01:51:35 -0500

    Se pasa a la segunda pantalla
    Se logra pasar a la segunda pantalla con una sola imagen

--- app/src/main/java/com/daniela/ultimaentrega/MainActivity.java
+++ app/src/main/java/com/daniela/ultimaentrega/MainActivity.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entrar=findViewById(R.id.Ingresar);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, Pantalla2.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -20,5 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(home);
             }
         },4000);
+
     }
+
 }
--- app/src/main/java/com/daniela/ultimaentrega/Pantalla2.java
+++ app/src/main/java/com/daniela/ultimaentrega/Pantalla2.java
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ListView;
 
 public class Pantalla2 extends AppCompatActivity {
 
+    ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
 
+        int [] idImagen= {
+                R.drawable.nacional,
+                R.drawable.economia,
+                R.drawable.deportes,
+                R.drawable.entretenimiento,
 
-    }
+    };
+}
 }
