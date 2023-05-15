1d1f748532d7e11419f6f3d42f6e5524c9f94b45
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-05-14 19:33:58 -0500

    PANTALLA SPLASH LISTO
    Se crea la pantalla de Splash

--- app/src/main/java/com/daniela/ultimaentrega/Pantalla2.java
+++ app/src/main/java/com/daniela/ultimaentrega/Pantalla2.java
@@ -2,7 +2,9 @@ package com.daniela.ultimaentrega;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Pantalla2 extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class Pantalla2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+
+
     }
 }
