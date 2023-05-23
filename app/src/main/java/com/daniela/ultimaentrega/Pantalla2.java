bfe85c31f3cb928341ca9be60f848f5089857cbf
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-05-22 20:00:21 -0500

    SE REALIZA CAMBIOS PARA PANTALLA 2
    .

--- app/src/main/java/com/daniela/ultimaentrega/Pantalla2.java
+++ app/src/main/java/com/daniela/ultimaentrega/Pantalla2.java
@@ -6,21 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Pantalla2 extends AppCompatActivity {
 
-    ListView lista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
 
-        int [] idImagen= {
-                R.drawable.deportes1,
-                R.drawable.economia1,
-                R.drawable.entretenimiento1,
-                R.drawable.internacional1,
-                R.drawable.tecno1,
+
     };
-}
+
 }
